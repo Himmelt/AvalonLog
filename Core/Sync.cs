a5252d294aa0a7d8c455fb92fcbd433c859fabda
Himmelt <master@void-3.cn>
2026-05-13 17:59:14 +0800

    refactor: 简化同步上下文安装的错误处理

--- Core/Sync.cs
+++ Core/Sync.cs
@@ -8,29 +8,17 @@ namespace AvalonLog.Core;
 internal static class SyncAvalonLog {
 
     private static SynchronizationContext? _ctx;
-    private static bool _errorFileWrittenOnce = false;
 
-    private static void InstallSynchronizationContext(bool logErrorsOnDesktop) {
+    private static void InstallSynchronizationContext() {
         if (SynchronizationContext.Current == null) {
-            SynchronizationContext.SetSynchronizationContext(
-                new DispatcherSynchronizationContext(Application.Current.Dispatcher));
+            SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext(Application.Current.Dispatcher));
         }
-        _ctx = SynchronizationContext.Current;
-
-        if (_ctx == null && logErrorsOnDesktop && !_errorFileWrittenOnce) {
-            string time = DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss-fff");
-            string filename = $"AvalonLog-同步上下文设置失败-{time}.txt";
-            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string file = System.IO.Path.Combine(desktop, filename);
-            try { System.IO.File.WriteAllText(file, "获取 DispatcherSynchronizationContext 失败"); } catch { }
-            _errorFileWrittenOnce = true;
-            throw new Exception("详见 " + file);
-        }
+        _ctx = SynchronizationContext.Current ?? throw new Exception("AvalonLog: DispatcherSynchronizationContext 获取失败");
     }
 
     public static SynchronizationContext Context {
         get {
-            if (_ctx == null) InstallSynchronizationContext(true);
+            if (_ctx == null) InstallSynchronizationContext();
             return _ctx!;
         }
     }
